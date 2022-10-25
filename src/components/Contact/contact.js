cf03d520a4f474148426cec0e2afe33ee177d06c
Kyle Patrick Bader <kbader91@gmail.com>
2022-10-25 16:02:00 -0700

    contact forms added at bottom of pages

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,9 +1,56 @@
 import './contact.css';
+import Fade from 'react-reveal/Fade';
+import Card from 'react-bootstrap/Card';
+import { BsEnvelope } from 'react-icons/bs';
 
 function Contact() {
     return (
         <section id="contact-page">
-            {/* <p>Hello World</p> */}
+            <div id="contact-container">
+                <Fade top>
+                    <div id="contact-header">
+                        Contact Us
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div id="contact-cards">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Aimee Klaus</Card.Title>
+                                <Card.Text>
+                                    +1 (773) 398-7881
+                                </Card.Text>
+                                <Card.Link href="mailto: Klausaimee31@gmail.com">
+                                    <BsEnvelope />
+                                </Card.Link>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Damion Smythia</Card.Title>
+                                <Card.Text>
+                                    +1 (213) 526-5675
+                                </Card.Text>
+                                <Card.Link href="mailto: Smythiadamion31@icloud.com">
+                                    <BsEnvelope />
+                                </Card.Link>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Joseph Munroe</Card.Title>
+                                <Card.Text>
+                                    +1 (239) 898-4248
+                                </Card.Text>
+                                <Card.Link href="mailto: jmunroe@steponedetox.com">
+                                    <BsEnvelope />
+                                </Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Fade>
+            </div>
+
         </section>
     )
 };
--- src/components/WWD/wwd.js
+++ src/components/WWD/wwd.js
@@ -3,8 +3,15 @@ import Fade from 'react-reveal/Fade';
 import Card from 'react-bootstrap/Card';
 import { BsHouseFill } from "react-icons/bs";
 import { FaHandHoldingUsd, FaTshirt } from "react-icons/fa";
+import { useState, useRef } from 'react';
 
 function WWD() {
+
+    const form = useRef();
+    const [contactForm, setContactForm] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = contactForm;
+    const [errorMessage, setErrorMessage] = useState('');
+
     return (
         <section id="wwd-page">
             <Fade top>
@@ -44,6 +51,31 @@ function WWD() {
                     </Card>
                 </div>
             </Fade>
+            <div id="bottom-contact">
+                <Fade left>
+                    <form id="contact-form">
+                        <p id="hp-contact-form-header">Reach Out</p>
+                        <div id="form-slot-1">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" defaultValue={name} name="name"/>
+                        </div>
+                        <div id="form-slot-2">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" defaultValue={email} name="email" />
+                        </div>
+                        <div id="form-slot-3">
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" defaultValue={message} rows="10" cols="45" />
+                        </div>
+                        <button type="submit" className="contact-submit-btn">Submit</button>
+                    </form>
+                </Fade>
+                <Fade right>
+                    <div id="google-map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3303.6489427455967!2d-118.3262758!3d34.10413270000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bf3ed11d5b7b%3A0x99f46e99e3954ee9!2s1800%20Vine%20St%20%23210%2C%20Los%20Angeles%2C%20CA%2090028!5e0!3m2!1sen!2sus!4v1666736738203!5m2!1sen!2sus" title="map" width="600" height="450" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </Fade>
+            </div>
         </section>
     )
 };
